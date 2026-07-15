54eb0cb42c870ce21aeb076605471c47facb3c32
TarLaboratories <levtaranov3@gmail.com>
2026-07-15 15:59:12 +0300

    fix

--- src/Vlad/dataset.py
+++ src/Vlad/dataset.py
@@ -3,14 +3,18 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
+
 class VSOSet(Dataset):
     def __init__(self, directory):
         directory = Path(directory)
         self.data = []
         for i in directory.iterdir():
+            print("Processing", i)
+            if i.is_dir():
+                continue
             df = pd.read_csv(i)
             cols = []
-            for i in 'ABCDEFGH':
+            for i in "ABCDEFGH":
                 if i in df.columns:
                     cols.append(i)
                 else:
@@ -18,16 +22,20 @@ class VSOSet(Dataset):
                         if (i + str(j)) in df.columns:
                             cols.append(i + str(j))
             if len(cols) == 0:
-                cols.append('total')
+                cols.append("total")
             for col in cols:
                 for i in df.index:
                     for j in df.index:
                         if j < i:
-                            self.data.append((df['name'][i], f['name'][j], df[col][i] > df[col][j]))
-                        else: break
+                            self.data.append(
+                                (df["name"][i], f["name"][j], df[col][i] > df[col][j])
+                            )
+                        else:
+                            break
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
         return self.data[index]
+
